3d6a994ef4885a0f7afd35c34c3819a61470e88b
Akshay Patil <118105416+PrinceAkshay3@users.noreply.github.com>
2023-07-13 00:07:08 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+class Main{
+    public static void main(String[] args) {
+
+    }
+
+    public static void checkNumber(int number){
+        if(number > 0){
+            System.out.println("positive");
+        }else if (number < 0){
+            System.out.println("negetive");
+        } else if (number == 0) {
+            System.out.println("zero");
+        }
+    }
+}
